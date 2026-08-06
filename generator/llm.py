3c323279295ea62dd53a79ec2f62ffb6cc51bf16
Darshan Gowda <dgowda112000@gmail.com>
2026-08-06 21:59:41 +0530

    model calling

--- generator/llm.py
+++ generator/llm.py
@@ -8,13 +8,16 @@ def generate_text(prompt: str) -> str:
     """
     Generate text using the LLM model """
     # Get the API key from the config module
-    api_key = config.get_required_env('openrouter_api_key')
-    base_url = 'https://openrouter.ai/api/v1/chat/completions'
-    gpt = OpenAI(api_key=api_key, base_url=base_url)
+    api_key = config.get_required_env('OPENROUTER_API_KEY')
+    base_url = 'https://openrouter.ai/api/v1'
+    gpt = OpenAI(api_key=api_key, base_url=base_url, timeout=30)
     response = gpt.chat.completions.create(
-    model=config.get_required_env('openrouter_model'),
+    model=config.get_required_env('OPENROUTER_MODEL'),
     messages=[{"role": "user", "content": prompt}],)
     return response.choices[0].message.content
 
 
-generate_text('Hi Buddy, How are you doing?')
+if __name__ == "__main__":
+    prompt = "Write a short story about a robot learning to love."
+    generated_text = generate_text(prompt)
+    print(generated_text)
